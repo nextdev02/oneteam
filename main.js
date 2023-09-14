27923c49b106a90d948467b2501822f86db33969
nextdev2 <nextdev0.02@gmail.com>
2023-09-14 14:35:23 +0600

    error fixing

---
+++ main.js
@@ -0,0 +1,40 @@
+// SHOW AND HIDE NAV MENU 
+
+const menu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// CLOSE NAV MENU 
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
+
+// Swiper JS
+
+let swipertestimonial = new Swiper(".testimonial-container", {
+    loop: true,
+    spaceBetween: 54,
+
+    
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    },
+    keyboard: true,
+});
+
